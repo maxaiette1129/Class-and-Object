6f7eac2c1819388f44740c0a3a36c3987c489ee5
maxai <maxai@MAX>
2023-04-19 08:57:04 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            user user1 = new user(); 
+            user1.firstName = "Max";
+            user1.lastName = "Cubero";
+            user1.address = "SFS";
+            user1.gender = "Male";
+            user1.age = 24;
+
+            user user2 = new user();
+            user2.firstName = "Glezel";
+            user2.lastName = "Cumla";
+            user2.address = "SFS";
+            user2.gender = "Female";
+            user2.age = 23;
+            
+        }
+    }
+}
---
+++ ConsoleApp1/user.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class user
+    {
+        public string firstName;
+       public string lastName;
+        public string gender;
+       public int age;
+        public string address;
+
+
+    }
+}
